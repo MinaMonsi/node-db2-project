d8dede5b5407f6edb6fdc251c573694b321f0c1f
Marina Monsivais <minamonsi@me.com>
2021-10-16 17:35:22 -0500

    Server running

--- api/server.js
+++ api/server.js
@@ -1,7 +1,12 @@
-const express = require("express")
+const express = require("express");
 
-const server = express()
+const server = express();
 
 // DO YOUR MAGIC
+server.use(express.json());
 
-module.exports = server
+server.use('*',(req,res)=>{
+    res.send("API is running");
+}); 
+
+module.exports = server;
